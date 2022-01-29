9114b10e4abd8bd1488b008af0e86fbe0461239f
Kumar, Nikhil <nikhilkumar9@deloitte.com>
2022-01-30 00:29:04 +0530

    https://www.interviewbit.com/problems/spiral-order-matrix-ii/

--- InterviewBit/array_matrix_spiral.py
+++ InterviewBit/array_matrix_spiral.py
@@ -85,3 +85,35 @@ def generateMatrix(A):
 
 a = generateMatrix(80)
 print(a)
+
+
+'''
+Fastest Solution
+
+
+class Solution:
+    # @param A : integer
+    # @return a list of list of integers
+    def generateMatrix(self, A):
+        l,r,t,b=0,A-1,0,A-1
+        ans=[[0]*A for _ in range(A)] 
+        num=1
+        while(l<=r and t<=b):
+            for i in range(l,r+1):
+                ans[t][i] = num
+                num+=1
+            t+=1
+            for i in range(t,b+1):
+                ans[i][r]= num
+                num+=1
+            r-=1
+            for i in range(r,l-1,-1):
+                ans[b][i] = num 
+                num+=1
+            b-=1
+            for i in range(b,t-1,-1):
+                ans[i][l] = num
+                num+=1
+            l+=1
+        return ans
+'''
